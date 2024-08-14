5b3830bff738b1c0e2dfe23d59a79570c0ce2aef
Rene Garcia <rsamuelgarcia@gmail.com>
2024-08-14 19:21:02 -0400

    fix

--- app/components/header/header_component.rb
+++ app/components/header/header_component.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Header
-  class Header::HeaderComponent < ViewComponent::Base
+  class HeaderComponent < ViewComponent::Base
     include Devise::Controllers::Helpers
   end
 end
